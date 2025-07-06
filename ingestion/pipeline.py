63f75e5e048a28a4c0b3bdd592e97faa2b8e967e
Gonzalo Ezama <ezama.gonzalo@gmail.com>
2025-07-06 18:53:32 +0200

    refactor: parametrization through pydantic and Fire

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -1,6 +1,7 @@
-import os
 from loguru import logger
+from fire import Fire
 
+from ingestion.models import IngestJobParameters
 from ingestion.download_ais_data import download_ais_data
 from ingestion.extract_zip_contents import extract_zip
 from utils.tree_clean_up import delete_folder
@@ -8,33 +9,24 @@ from ingestion.extract_zip_contents import extract_zip
 from ingestion.t_into_parquet import lazy_csv_to_parquet
 
 
-def main_pipeline(download_output_dir, csv_output_dir, parquet_output_dir):
+def ingest_pipeline(params: IngestJobParameters):
 
     logger.info("Starting ingestion pipeline")
     for f_path in download_ais_data(
-        start_date="2024-10-10", end_date="2024-10-10", output_dir=download_output_dir
+        start_date=params.start_date, end_date=params.end_date, output_dir=params.download_output_dir
     ):
-        for extracted_csv in extract_zip(f_path, csv_output_dir):
+        for extracted_csv in extract_zip(f_path, params.csv_output_dir):
             lazy_csv_to_parquet(
                 extracted_csv,
-                output_path=parquet_output_dir,
-                datetime_col="# Timestamp",
-                dt_format="%d/%m/%Y %H:%M:%S",
+                output_path=params.parquet_output_dir,
+                datetime_col=params.datetime_col,
+                dt_format=params.dt_format,
             )
 
-    delete_folder(download_output_dir)
-    delete_folder(csv_output_dir)
+    delete_folder(params.download_output_dir)
+    delete_folder(params.csv_output_dir)
     logger.info("Finished ingestion pipeline")
 
 
 if __name__ == "__main__":
-
-    download_output_dir = "./ais_downloaded_data"
-    csv_output_dir = "./ais_csv_data"
-    parquet_output_dir = "./ais_data"
-
-    main_pipeline(
-        download_output_dir=download_output_dir,
-        csv_output_dir=csv_output_dir,
-        parquet_output_dir=parquet_output_dir,
-    )
+    Fire(lambda **kwargs: ingest_pipeline(IngestJobParameters(**kwargs)))
